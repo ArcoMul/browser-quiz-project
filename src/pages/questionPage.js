e9c3863fbfeabae6660e50348178c0acf3ba259f
Beyza Gok <beyzagoknl@gmail.com>
2022-06-23 15:27:41 +0200

    added sounds for correct and wrong answers

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -1,77 +1,87 @@
 'use strict';
 
 import {
-  ANSWERS_LIST_ID,
-  NEXT_QUESTION_BUTTON_ID,
-  USER_INTERFACE_ID,
+    ANSWERS_LIST_ID,
+    NEXT_QUESTION_BUTTON_ID,
+    USER_INTERFACE_ID,
 } from '../constants.js';
 import {
-  createQuestionElement,
-  createProgressElement,
+    createQuestionElement,
+    createProgressElement,
 } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
 import { createAlertElement } from '../views/questionView.js';
 
-
-let currentAnswerElement= [];
+let currentAnswerElement = [];
 export const initQuestionPage = () => {
-  const userInterface = document.getElementById(USER_INTERFACE_ID);
-  userInterface.innerHTML = '';
+    const userInterface = document.getElementById(USER_INTERFACE_ID);
+    userInterface.innerHTML = '';
 
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-  document.title = currentQuestion.text.substring(0, 60) + '...';
-  const questionElement = createQuestionElement(currentQuestion.text);
-  const userProgress = createProgressElement(
-    quizData.questions.length,
-    quizData.currentQuestionIndex + 1,
-    0
-  );
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    document.title = currentQuestion.text.substring(0, 60) + '...';
+    const questionElement = createQuestionElement(currentQuestion.text);
+    const userProgress = createProgressElement(
+        quizData.questions.length,
+        quizData.currentQuestionIndex + 1,
+        0
+    );
 
-  userInterface.appendChild(userProgress);
-  userInterface.appendChild(questionElement);
+    userInterface.appendChild(userProgress);
+    userInterface.appendChild(questionElement);
 
-  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
-  for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
-    const answerElement = createAnswerElement(key, answerText);
-    answersListElement.appendChild(answerElement);
-    answerElement.addEventListener('click', function () {
-      quizData.currentQuestionAnswer = key;
-      currentAnswerElement = answerElement;
-      answersListElement
-        .querySelectorAll('.selected')
-        .forEach((element) => element.classList.remove('selected'));
-      answerElement.classList.add('selected');
-    });
-  }
+    for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
+        const answerElement = createAnswerElement(key, answerText);
+        answersListElement.appendChild(answerElement);
+        answerElement.addEventListener('click', function () {
+            quizData.currentQuestionAnswer = key;
+            currentAnswerElement = answerElement;
+            answersListElement
+                .querySelectorAll('.selected')
+                .forEach((element) => element.classList.remove('selected'));
+            answerElement.classList.add('selected');
+        });
+    }
 
-  document
-    .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
+    document
+        .getElementById(NEXT_QUESTION_BUTTON_ID)
+        .addEventListener('click', nextQuestion);
 };
 
 const nextQuestion = () => {
-  const correctAnswer = quizData.questions[quizData.currentQuestionIndex].correct;
-  const addClass = quizData.currentQuestionAnswer === correctAnswer ? 'correct' : 'wrong';
-  const body = document.getElementById(USER_INTERFACE_ID);
-  if (quizData.currentQuestionAnswer===null) {
-    const alertElement = createAlertElement();
-    body.appendChild(alertElement);
-    quizData.currentQuestionIndex = quizData.currentQuestionIndex -1;
-  } else if(quizData.currentQuestionAnswer === correctAnswer ) {
-    currentAnswerElement.classList.remove('selected')
-    currentAnswerElement.classList.add(addClass)
-  } else {
-    currentAnswerElement.classList.remove('selected')
-    currentAnswerElement.classList.add(addClass)
-  }
-  quizData.currentQuestionAnswer = null
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
-  document.getElementById(NEXT_QUESTION_BUTTON_ID).removeEventListener('click', nextQuestion);
+    const correctAnswer =
+        quizData.questions[quizData.currentQuestionIndex].correct;
+    const addClass =
+        quizData.currentQuestionAnswer === correctAnswer ? 'correct' : 'wrong';
+    const body = document.getElementById(USER_INTERFACE_ID);
+    const soundCorrect = new Audio('../../public/sounds/sound_correct.mp3');
+    const soundWrong = new Audio('../../public/sounds/sound_wrong.mp3');
 
-  setTimeout(() => {
-    initQuestionPage();
-    currentAnswerElement.classList.remove(addClass)
-  }, 1500);
+    if (quizData.currentQuestionAnswer === null) {
+        const alertElement = createAlertElement();
+        body.appendChild(alertElement);
+        quizData.currentQuestionIndex = quizData.currentQuestionIndex - 1;
+    } else if (quizData.currentQuestionAnswer === correctAnswer) {
+        currentAnswerElement.classList.remove('selected');
+        currentAnswerElement.classList.add(addClass);
+        soundCorrect.play();
+    } else {
+        currentAnswerElement.classList.remove('selected');
+        currentAnswerElement.classList.add(addClass);
+        soundWrong.play();
+    }
+    quizData.currentQuestionAnswer = null;
+    quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+    document
+        .getElementById(NEXT_QUESTION_BUTTON_ID)
+        .removeEventListener('click', nextQuestion);
+
+    setTimeout(() => {
+        initQuestionPage();
+        currentAnswerElement.classList.remove(addClass);
+        soundCorrect.pause();
+        soundWrong.pause();
+    }, 1500);
 };
