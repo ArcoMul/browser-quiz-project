c5859ae7ff8622f3586259f884890b09e0aa53f2
Mones Hamd <mones.m.azzam91@gmail.com>
2022-06-24 15:03:21 +0200

    add animation back ground

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
+import { drawText } from './pages/canvasPage.js';
 
 const loadApp = () => {
   quizData.currentQuestionIndex = 0;
 
   initWelcomePage();
+  setInterval(drawText, 50);
 };
 
 window.addEventListener('load', loadApp);
